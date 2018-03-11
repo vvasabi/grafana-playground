b22d7415affa625f0fed6fe5e46dcca10fd752ed
Brad Chen <brad@bradchen.com>
2018-03-10 21:18:07 -0500

    Use SpringApplication.exit to stop application.

--- src/main/java/com/bc/jenkins/integration/playground/PlaygroundApplication.java
+++ src/main/java/com/bc/jenkins/integration/playground/PlaygroundApplication.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class PlaygroundApplication implements CommandLineRunner {
+    @Autowired
+    private ApplicationContext appContext;
 
     @Autowired
     private GrafanaService grafanaService;
@@ -21,7 +24,7 @@ public class PlaygroundApplication implements CommandLineRunner {
         IntStream.range(0, args.length)
             .forEach(i -> System.out.println(i + ": " + args[i]));
         grafanaService.updateMessage("foo bar" + (args.length > 0 ? " " + args[0] : ""));
-        System.exit(0);
+        SpringApplication.exit(appContext, () -> 0);
     }
 
     public static void main(String[] args) {
